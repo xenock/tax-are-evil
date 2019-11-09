cb0b59f9aad67fe4cef32968fdadc4f7bb3dcc02
xenock <miguelmoracho@gmail.com>
2019-11-09 14:42:51 +0100

    feat: initial state

---
+++ src/common/i18n.js
@@ -0,0 +1,3 @@
+export default {
+    REACTOR: 'I hate taxes'
+}
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export default {
+	  "inputFields":  [
+		    {
+			      "id":  "name",
+			      "label":  "Name",
+			      "placeholder":  "Your first name",
+			      "type":  "text",
+			      "maxLength":  20
+		    },
+		    {
+			      "id":  "surname",
+			      "label":  "Surname",
+			      "placeholder":  "Your last name",
+			      "type":  "text",
+			      "maxLength":  40
+		    },
+		    {
+			      "id":  "age",
+			      "label":  "Age",
+			      "placeholder":  "Your age",
+			      "type":  "number",
+		    }
+	  ]
+}
